507d59ce088c9ec0b5f973a762da16ae5f347c39
amandalynn1204 <amandalynn1204@icloud.com>
2024-01-27 13:52:01 -0500

    added JavaScript functionality

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+function decreaseCount() {
+  count--;
+  countDisplay.innerHTML = count;
+}
 
+function resetCount() {
+  count = 0;
+  countDisplay.innerHTML = count;
+}
+
+function increaseCount() {
+  count++;
+  countDisplay.innerHTML = count;
+}
+
+const decreaseButton = document.getElementById("decrease-button");
+const resetButton = document.getElementById("reset-button");
+const increaseButton = document.getElementById("increase-button");
+const countDisplay = document.getElementById("count-display");
+let count = 0;
+countDisplay.innerHTML = count;
+
+decreaseButton.addEventListener("click", decreaseCount);
+resetButton.addEventListener("click", resetCount);
+increaseButton.addEventListener("click", increaseCount);
