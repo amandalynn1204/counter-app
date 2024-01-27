52dd7ae69bb051ebabe8baf0fda8624ace2fac74
amandalynn1204 <amandalynn1204@icloud.com>
2024-01-27 14:15:57 -0500

    added styling to count display through JavaScript

--- src/app.js
+++ src/app.js
@@ -1,16 +1,34 @@
 function decreaseCount() {
   count--;
   countDisplay.innerHTML = count;
+
+  if (count < 0) {
+    countDisplay.classList.add("red");
+  }
+  if (count === 0) {
+    countDisplay.classList.remove("red");
+    countDisplay.classList.remove("green");
+  }
 }
 
 function resetCount() {
   count = 0;
   countDisplay.innerHTML = count;
+  countDisplay.classList.remove("red");
+  countDisplay.classList.remove("green");
 }
 
 function increaseCount() {
   count++;
   countDisplay.innerHTML = count;
+
+  if (count > 0) {
+    countDisplay.classList.add("green");
+  }
+  if (count === 0) {
+    countDisplay.classList.remove("green");
+    countDisplay.classList.remove("red");
+  }
 }
 
 const decreaseButton = document.getElementById("decrease-button");
